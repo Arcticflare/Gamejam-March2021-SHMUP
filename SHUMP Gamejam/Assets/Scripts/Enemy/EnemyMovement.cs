ffd925f26cfcea492b5cd3b9e275fe811da49c7c
Matt <arcticflare69@gmail.com>
2021-03-29 13:49:21 +1300

    cleaned up

--- SHUMP Gamejam/Assets/Scripts/Enemy/EnemyMovement.cs
+++ SHUMP Gamejam/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     Rigidbody2D rb;
-    float left;
-    float right;
-    float down;
-    float up;
 
     [SerializeField]
     float moveTime = 2;
@@ -16,16 +12,11 @@ public class EnemyMovement : MonoBehaviour
     float speed = 5.0f;
     float elapsedTime = 0.0f;
     Vector2 direction;
-    bool moving;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
-        right = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
-        left = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x;
-        up = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y;
-        down = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y;
     }
 
     void Update()
